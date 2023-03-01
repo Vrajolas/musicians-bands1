03d95deda28578e2f0892254b5b1cb5ec69ac682
Vrajolas <vrajeshrajpro11@gmail.com>
2023-03-01 14:39:55 +0000

    One-to-Many association has been added and tests have been written for it

--- index.test.js
+++ index.test.js
@@ -31,4 +31,34 @@ describe('Band and Musician Models', () => {
         expect(musician.name).toBe('John Lennon');
         expect(musician.instrument).toBe('Guitar');
     })
+
+    test('can add musicians to a band', async () => {
+        const band = await Band.create({
+          name: 'The Beatles',
+          genre: 'Rock',
+        });
+    
+        const musician1 = await Musician.create({
+          name: 'John Lennon',
+          instrument: 'Guitar',
+        });
+
+        const musician2 = await Musician.create({
+          name: 'George Harrison',
+          instrument: 'Guitar',
+        });
+
+        await band.addMusicians([musician1, musician2])
+
+        const bands = await Band.findAll();
+        expect(band.length).toBe(1);
+
+        const foundBand = bands[0];
+        const musicians = await foundBand.getMusicians();
+        expect(musicians.length).toBe(4);
+        expect(musicians.map((m) => m.name)).toEqual([
+            'John Lennon',
+            'George Harrison'
+        ])
+    })
 })
